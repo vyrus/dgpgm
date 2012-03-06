e146da71cf8d1ce5c14b0d402d2fa2f64f701dd1
Zvezdochka <x4x@bk.ru>
2012-03-06 04:48:03 +0400

    Changed to EnhacedGrid with pagination to display a lot of items in grid for report "course" .

--- tpl/cms/stats/course.php
+++ tpl/cms/stats/course.php
@@ -21,26 +21,26 @@ include TPL_CMS."_header.php";
 
 <h2>Выводимые показатели</h2>
 
-<input type="checkbox" name="tender_count" value="1" checked="checked" /> Количество конкурсов (план)<br /><br />
+<input type="checkbox" name="tender_count" value="1" checked="checked" /> Количество конкурсов (план)<br />
 
-<input type="checkbox" name="tend_num_podacha" value="1" checked="checked" /> Количество конкурсов на этапе подачи заявок<br /><br />
+<input type="checkbox" name="tend_num_podacha" value="1" checked="checked" /> Количество конкурсов на этапе подачи заявок<br />
 
-<input type="checkbox" name="tend_num_rassmotr" value="1" /> Количество конкурсов на этапе рассмотрения<br /><br />
+<input type="checkbox" name="tend_num_rassmotr" value="1" /> Количество конкурсов на этапе рассмотрения<br />
 
-<input type="checkbox" name="tend_num_commit" value="1" checked="checked" /> Проведено конкурсов<br /><br />
+<input type="checkbox" name="tend_num_commit" value="1" checked="checked" /> Проведено конкурсов<br />
 
 <?/*<input type="checkbox" name="total_funding" value="1" /> Общее финансирование на <?=date('Y')?>, тыс. руб.<br /><br />*/?>
 
-<input type="checkbox" name="tender_commited_money" value="1" /> Сумма проведенных конкурсов, тыс. руб.<br /><br />
+<input type="checkbox" name="tender_commited_money" value="1" /> Сумма проведенных конкурсов, тыс. руб.<br />
 
-<input type="checkbox" name="winners_money" value="1" /> Сумма заявок победителей, тыс. руб.<br /><br />
+<input type="checkbox" name="winners_money" value="1" /> Сумма заявок победителей, тыс. руб.<br />
 
-<input type="checkbox" name="economy" value="1" checked="checked" /> Экономия средств по проведенным конкурсам в <?=date('Y')?> году, тыс. руб.<br /><br />
+<input type="checkbox" name="economy" value="1" checked="checked" /> Экономия средств по проведенным конкурсам в <?=date('Y')?> году, тыс. руб.<br />
 
 
 <h2>Параметры вывода</h2>
 
-<input type="checkbox" name="detail_by_measures" value="1" /> Детализировать по мероприятиям<br /><br />
+<input type="checkbox" name="detail_by_measures" value="1" /> Детализировать по мероприятиям<br />
 
 <input type="submit" value="Сформировать отчет">
 
